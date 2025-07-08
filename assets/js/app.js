3a7b11a7f058150c27d949cb88ca5c2e4499a01f
somemusic <veritempago@gmail.com>
2025-07-08 20:36:45 +0200

    Change page size

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,7 +5,7 @@ let allTags = []; // Comment and uncomment below if local
 let allTag = { "label": "ALL", "couleur": "#000000" }
 
 let currentPage = 1;
-const pageSize = 5;
+const pageSize = 10;
 let filteredTracks = [];
 let filteredTags = [allTag];
 
